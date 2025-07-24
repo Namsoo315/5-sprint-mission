c7f1429643161ec0788741cf6decf45e1cb00d51
Namsoo315 <gustn2130@naver.com>
2025-07-24 17:29:10 +0900

    feat : add File*Service 비즈니스 로직 구현체

--- src/main/java/com/sprint/mission/discodeit/service/file/FileChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileChannelService.java
@@ -1,4 +1,67 @@
 package com.sprint.mission.discodeit.service.file;
 
-public class FileChannelService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+
+public class FileChannelService implements ChannelService {
+	ChannelRepository repo;
+
+	public FileChannelService(ChannelRepository repo) {
+		this.repo = repo;
+	}
+
+	@Override
+	public Channel createChannel(String name, String description) {
+		Channel channel = new Channel(name, description);
+		repo.save(channel);
+
+		return channel;
+	}
+
+	@Override
+	public Optional<Channel> findById(UUID uuid) {
+		return repo.findById(uuid);
+	}
+
+	@Override
+	public List<Channel> findByChannelName(String name) {
+		List<Channel> list = new ArrayList<>();
+
+		for (Channel channel : repo.findAll()){
+			if (channel.getName().equals(name)){
+				list.add(channel);
+			}
+		}
+
+		return list;
+	}
+
+	@Override
+	public List<Channel> findByAllChannel() {
+		return repo.findAll();
+	}
+
+	@Override
+	public void updateChannel(UUID uuid, String name, String description) {
+		Channel channel = repo.findById(uuid).orElse(null);
+
+		if(channel == null) {
+			throw new IllegalArgumentException("채널이 존재하지 않습니다.");
+		}
+
+		channel.setName(name);
+		channel.setDescription(description);
+		repo.save(channel);
+	}
+
+	@Override
+	public void deleteChannel(UUID uuid) {
+		repo.delete(uuid);
+	}
 }
--- src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
@@ -1,4 +1,52 @@
 package com.sprint.mission.discodeit.service.file;
 
-public class FileUserService {
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.UserService;
+
+public class FileUserService implements UserService {
+	UserRepository repo;
+
+	public FileUserService(UserRepository repo) {
+		this.repo = repo;
+	}
+
+	@Override
+	public User createUser(String name, int age) {
+		User user = new User(name, age);
+		repo.save(user);
+		return user;
+	}
+
+	@Override
+	public Optional<User> findById(UUID uuid) {
+		return repo.findById(uuid);
+	}
+
+	@Override
+	public List<User> findByAll() {
+		return repo.findAll();
+	}
+
+	@Override
+	public void updateUser(UUID uuid, String username, int age) {
+		User user = repo.findById(uuid).orElse(null);
+
+		if (user == null) {
+			throw new IllegalArgumentException("아이디가 존재하지 않습니다.");
+		}
+
+		user.setAge(age);
+		user.setUsername(username);
+		repo.save(user);
+	}
+
+	@Override
+	public void deleteUser(UUID uuid) {
+		repo.delete(uuid);
+	}
 }
