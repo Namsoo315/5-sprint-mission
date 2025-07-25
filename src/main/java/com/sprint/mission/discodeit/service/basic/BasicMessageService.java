88e860c5df2ac4194f426e183443ca30b0da8f5b
Namsoo315 <gustn2130@naver.com>
2025-07-25 22:34:31 +0900

    feat : Basic*Service add 비즈니스 로직과 저장 로직을 분리 후 각자의 역할만 수행하게 끔 실행하게 만듬.

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -0,0 +1,71 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.service.MessageService;
+
+public class BasicMessageService implements MessageService {
+	private final MessageRepository repo;
+
+	public BasicMessageService(MessageRepository repo) {
+		this.repo = repo;
+	}
+
+	@Override
+	public Message createMessage(UUID userId, UUID channelId, String content) {
+
+		if (userId == null) {
+			throw new IllegalArgumentException("유저를 찾을 수 없습니다.");
+		}
+		if (channelId == null) {
+			throw new IllegalArgumentException("채널방을 찾을 수 없습니다.");
+		}
+
+		Message message = new Message(userId, channelId, content);
+		repo.save(message);
+
+		return message;
+	}
+
+	@Override
+	public List<Message> findByUserIdAndChannelId(UUID userId, UUID channelId) {
+		List<Message> result = new ArrayList<>();
+		for (Message m : repo.findAll()) {
+			if (m.getUserId().equals(userId) && m.getChannelId().equals(channelId)) {
+				result.add(m);
+			}
+		}
+
+		return result;
+	}
+
+	@Override
+	public Optional<Message> findByMessage(UUID messageId) {
+		return repo.findById(messageId);
+	}
+
+	public List<Message> findByAllMessage() {
+		return repo.findAll();
+	}
+
+	@Override
+	public void updateMessage(UUID messageId, String newContent) {
+		Message message = repo.findById(messageId).orElse(null);
+
+		if(message == null) {
+			throw new IllegalArgumentException("메시지를 찾을 수 없습니다.");
+		}
+		message.setMessage(newContent);
+		repo.save(message);
+	}
+
+	@Override
+	public void deleteMessage(UUID messageId) {
+		repo.delete(messageId);
+	}
+}
