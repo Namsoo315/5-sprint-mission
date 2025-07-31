0ed598905daedda872f372c5e2a30363af1f4c86
Namsoo315 <gustn2130@naver.com>
2025-07-31 11:38:17 +0900

    feat : field add (name, email find)

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -2,7 +2,6 @@ package com.sprint.mission.discodeit.entity;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.UUID;
 
@@ -15,31 +14,34 @@ public class User implements Serializable {
 	private final long serialVersionUID = 1L;
 	private final UUID userId;
 	private String username;
-	private int age;
+	private String email;
+	private String password;
 	private final Instant createdAt;
 	private Instant updatedAt;
 
-	public User(String name, int age) {
+	public User(String name, String email, String password) {
 		this.userId = UUID.randomUUID();
 		this.username = name;
-		this.age = age;
+		this.email = email;
+		this.password = password;
 		this.createdAt = Instant.now();
 		this.updatedAt = createdAt;
 	}
 
-	public void update(String username, int age){
+	public void update(String username,String email, String age){
 		this.username = username;
-		this.age = age;
+		this.email = email;
+		this.password = age;
 		this.updatedAt = Instant.now();
 	}
 
 	@Override
 	public String toString() {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		final StringBuilder sb = new StringBuilder("User[");
 		sb.append(" uuid=").append(userId).append('\'');
 		sb.append(", username=").append(username).append('\'');
-		sb.append(", age=").append(age).append('\'');
+		sb.append(", email=").append(email).append('\'');
+		sb.append(", age=").append(password).append('\'');
 		sb.append(", createdAt=").append(createdAt).append('\'');
 		sb.append(", updatedAt=").append(updatedAt);
 		sb.append(" ]");
