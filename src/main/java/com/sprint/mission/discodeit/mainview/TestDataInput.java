03249ad7eb28a27bb58ef1d5ed157d32e440122c
Namsoo315 <gustn2130@naver.com>
2025-07-31 19:01:53 +0900

    docs: change create field

--- src/main/java/com/sprint/mission/discodeit/mainview/TestDataInput.java
+++ src/main/java/com/sprint/mission/discodeit/mainview/TestDataInput.java
@@ -1,7 +1,9 @@
 package com.sprint.mission.discodeit.mainview;
 
-import com.sprint.mission.discodeit.dto.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.channel.PublicChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.user.UserCreateRequest;
 import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
@@ -13,8 +15,8 @@ public class TestDataInput {
 		User user2 = userService.createUser(new UserCreateRequest("남현수2", "namsoo2@test2.com", "1234"));
 		User user3 = userService.createUser(new UserCreateRequest("남현수3", "namsoo3@test3.com", "1234"));
 
-		Channel channel1 = channelService.createChannel("채널 1", "스터디 공부방입니다.");
-		Channel channel2 = channelService.createChannel("채널 2", "커뮤니티 노는방입니다.");
+		Channel channel1 = channelService.createPublicChannel(new PublicChannelCreateRequest(ChannelType.PUBLIC,"채널 1", "스터디 공부방입니다."));
+		Channel channel2 = channelService.createPublicChannel(new PublicChannelCreateRequest(ChannelType.PUBLIC,"채널 2", "커뮤니티 노는방입니다."));
 
 		messageService.createMessage(user1.getUserId(), channel2.getChannelId(), "user1의 두번째 메시지");
 		messageService.createMessage(user1.getUserId(), channel2.getChannelId(), "user1의 세번째 메시지");
