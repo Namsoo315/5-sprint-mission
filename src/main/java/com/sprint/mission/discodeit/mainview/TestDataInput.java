a38f33bdc3d11d89f3301cae55988e87875ae850
Namsoo315 <gustn2130@naver.com>
2025-08-01 19:27:54 +0900

    docs : test Data 수정

--- src/main/java/com/sprint/mission/discodeit/mainview/TestDataInput.java
+++ src/main/java/com/sprint/mission/discodeit/mainview/TestDataInput.java
@@ -18,11 +18,11 @@ public class TestDataInput {
 		Channel channel1 = channelService.createPublicChannel(new PublicChannelCreateRequest("채널 1", "스터디 공부방입니다."));
 		Channel channel2 = channelService.createPublicChannel(new PublicChannelCreateRequest("채널 2", "커뮤니티 노는방입니다."));
 
-		messageService.createMessage(user1.getUserId(), channel2.getChannelId(), "user1의 두번째 메시지");
-		messageService.createMessage(user1.getUserId(), channel2.getChannelId(), "user1의 세번째 메시지");
-		messageService.createMessage(user2.getUserId(), channel1.getChannelId(), "user2의 첫번째 메시지");
-		messageService.createMessage(user2.getUserId(), channel2.getChannelId(), "user2의 두번째 메시지");
-		messageService.createMessage(user3.getUserId(), channel1.getChannelId(), "user3의 첫번째 메시지");
+		// messageService.createMessage(user1.getUserId(), channel2.getChannelId(), "user1의 두번째 메시지");
+		// messageService.createMessage(user1.getUserId(), channel2.getChannelId(), "user1의 세번째 메시지");
+		// messageService.createMessage(user2.getUserId(), channel1.getChannelId(), "user2의 첫번째 메시지");
+		// messageService.createMessage(user2.getUserId(), channel2.getChannelId(), "user2의 두번째 메시지");
+		// messageService.createMessage(user3.getUserId(), channel1.getChannelId(), "user3의 첫번째 메시지");
 	}
 
 }
