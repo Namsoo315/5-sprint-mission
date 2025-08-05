9e34b201cb6aa7cef3e06264be665e399cd4452f
Namsoo315 <gustn2130@naver.com>
2025-08-05 15:19:27 +0900

    docs: BinaryContent 분리

--- src/main/java/com/sprint/mission/discodeit/dto/user/UserCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/user/UserCreateRequest.java
@@ -1,7 +1,5 @@
 package com.sprint.mission.discodeit.dto.user;
 
-import java.util.UUID;
-
 import com.sprint.mission.discodeit.entity.BinaryContent;
 
 import lombok.AllArgsConstructor;
@@ -14,26 +12,11 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class UserCreateRequest {
-	private UUID userId;
 	private String username;
 	private String email;
 	private String password;
-	private BinaryContent binaryContent;
-
-	public UserCreateRequest(String username, String email, String password) {
-		this.username = username;
-		this.email = email;
-		this.password = password;
-	}
-
-	@Override
-	public String toString() {
-		return "UserCreateRequest{" +
-			"userId=" + userId +
-			", username='" + username + '\'' +
-			", email='" + email + '\'' +
-			", age=" + password +
-			", content=" + binaryContent +
-			'}';
-	}
+	private String fileName;
+	private String contentType;
+	private Long size;
+	private byte[] binaryContent;
 }
