d5ec02b22bf20af32dccef3b4ff63eacde5992f6
Namsoo315 <gustn2130@naver.com>
2025-08-05 15:22:10 +0900

    docs: 도메인 재정의 DTO 재정의

--- src/main/java/com/sprint/mission/discodeit/dto/binary/BinaryContentCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/binary/BinaryContentCreateRequest.java
@@ -1,8 +1,5 @@
 package com.sprint.mission.discodeit.dto.binary;
 
-import java.util.List;
-import java.util.UUID;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class BinaryContentCreateRequest {
-	private UUID BinaryContentId;
-	private List<UUID> attachmentIds;
 	private String fileName;
 	private String contentType;
 	private Long size;
