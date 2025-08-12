4a9ba5d69ebb616fa2bbc7d8092d7d6f303ae99b
Namsoo315 <gustn2130@naver.com>
2025-08-12 15:57:03 +0900

    feat: Login Controller 구현

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -2,22 +2,29 @@ package com.sprint.mission.discodeit.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.sprint.mission.discodeit.dto.auth.AuthLoginRequest;
 import com.sprint.mission.discodeit.dto.auth.AuthLoginResponse;
+import com.sprint.mission.discodeit.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
-	// [ ] 사용자는 로그인할 수 있다.
-	@RequestMapping("/")
-	public ResponseEntity<AuthLoginResponse> login(){
+	private final AuthService authService;
 
-		return ResponseEntity.status(HttpStatus.OK).body(null);
+	// [ ] 사용자는 로그인할 수 있다.
+	@RequestMapping(path = "/", method = RequestMethod.POST)
+	public ResponseEntity<AuthLoginResponse> login(@RequestBody AuthLoginRequest request) {
+		AuthLoginResponse result = authService.login(request);
+
+		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
 }
