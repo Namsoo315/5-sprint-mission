bc3adbf41d20f4d7c7e9c9d951e196554bc9e767
Namsoo315 <gustn2130@naver.com>
2025-08-21 17:49:45 +0900

    docs: Controller Swagger 사용, HTTP 응답에 맞게 description 추가

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -5,79 +5,93 @@ import com.sprint.mission.discodeit.dto.message.MessageCreateRequest;
 import com.sprint.mission.discodeit.dto.message.MessageUpdateRequest;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.service.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/messages")
-@Tag(name = "message", description = "메시지 관련 API")
+@Tag(name = "Message", description = "메시지 관련 API")
 public class MessageController {
 
-  private final MessageService messageService;
+    private final MessageService messageService;
 
-  // [ ] 메시지 전송
-  @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  public ResponseEntity<Message> sendMessage(
-      @RequestPart MessageCreateRequest messageCreateRequest,
-      @RequestPart(required = false) List<MultipartFile> attachments) throws IOException {
+    // [ ] 메시지 전송
+    @Operation(summary = "메시지 전송 API", responses = {
+            @ApiResponse(responseCode = "201", description = "메시지가 정상적으로 전송되었습니다."),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 값입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Message> sendMessage(
+            @RequestPart MessageCreateRequest messageCreateRequest,
+            @RequestPart(required = false) List<MultipartFile> attachments) throws IOException {
 
-    List<BinaryContentDTO> binaryContents = new ArrayList<>();
+        List<BinaryContentDTO> binaryContents = new ArrayList<>();
 
-    if (attachments != null && !attachments.isEmpty()) {
-      for (MultipartFile file : attachments) {
-        binaryContents.add(new BinaryContentDTO(
-            file.getOriginalFilename(),
-            file.getContentType(),
-            file.getSize(),
-            file.getBytes()
-        ));
-      }
+        if (attachments != null && !attachments.isEmpty()) {
+            for (MultipartFile file : attachments) {
+                binaryContents.add(new BinaryContentDTO(
+                        file.getOriginalFilename(),
+                        file.getContentType(),
+                        file.getSize(),
+                        file.getBytes()
+                ));
+            }
+        }
+
+        Message message = messageService.createMessage(messageCreateRequest, binaryContents);
+        return ResponseEntity.status(HttpStatus.CREATED).body(message); // 201 Created
     }
 
-    Message message = messageService.createMessage(messageCreateRequest, binaryContents);
-    return ResponseEntity.status(HttpStatus.CREATED).body(message); // 201 Created
-  }
+    // [ ] 메시지 수정
+    @Operation(summary = "메시지 수정 API", responses = {
+            @ApiResponse(responseCode = "200", description = "메시지가 정상적으로 수정되었습니다."),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 값입니다."),
+            @ApiResponse(responseCode = "404", description = "메시지를 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @PatchMapping("/{messageId}")
+    public ResponseEntity<Message> modifyMessage(
+            @PathVariable UUID messageId,
+            @RequestBody MessageUpdateRequest messageUpdateRequest) {
+        Message message = messageService.updateMessage(messageId, messageUpdateRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(message); // 200 OK
+    }
 
-  // [ ] 메시지 수정
-  @PatchMapping("/{messageId}")
-  public ResponseEntity<Message> modifyMessage(
-      @PathVariable UUID messageId,
-      @RequestBody MessageUpdateRequest messageUpdateRequest) {
-    Message message = messageService.updateMessage(messageId, messageUpdateRequest);
-    return ResponseEntity.status(HttpStatus.OK).body(message); // 200 OK
-  }
+    // [ ] 메시지 삭제
+    @Operation(summary = "메시지 삭제 API", responses = {
+            @ApiResponse(responseCode = "204", description = "메시지가 정상적으로 삭제되었습니다."),
+            @ApiResponse(responseCode = "404", description = "메시지를 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @DeleteMapping("/{messageId}")
+    public ResponseEntity<Void> deleteMessage(@PathVariable UUID messageId) {
+        messageService.deleteMessage(messageId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); // 204 No Content
+    }
 
-  // [ ] 메시지 삭제
-  @DeleteMapping("/{messageId}")
-  public ResponseEntity<String> deleteMessage(@PathVariable UUID messageId) {
-    messageService.deleteMessage(messageId);
-    return ResponseEntity.status(HttpStatus.NO_CONTENT)
-        .body(messageId + "님의 메시지 삭제 완료"); // 204 No Content
-  }
-
-  // [ ] 특정 채널의 메시지 조회
-  @GetMapping
-  public ResponseEntity<List<Message>> findMessageByChannelId(@RequestParam UUID channelId) {
-    List<Message> messages = messageService.findAllByChannelId(channelId);
-    return ResponseEntity.status(HttpStatus.OK).body(messages); // 200 OK
-  }
+    // [ ] 특정 채널의 메시지 조회
+    @Operation(summary = "채널별 메시지 조회 API", responses = {
+            @ApiResponse(responseCode = "200", description = "메시지가 정상적으로 조회되었습니다."),
+            @ApiResponse(responseCode = "404", description = "채널을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @GetMapping
+    public ResponseEntity<List<Message>> findMessageByChannelId(@RequestParam UUID channelId) {
+        List<Message> messages = messageService.findAllByChannelId(channelId);
+        return ResponseEntity.status(HttpStatus.OK).body(messages); // 200 OK
+    }
 }
--- src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -4,20 +4,16 @@ import com.sprint.mission.discodeit.dto.readstatus.ReadStatusCreateRequest;
 import com.sprint.mission.discodeit.dto.readstatus.ReadStatusUpdateRequest;
 import com.sprint.mission.discodeit.entity.ReadStatus;
 import com.sprint.mission.discodeit.service.ReadStatusService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,30 +21,45 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "ReadStatus", description = "수신 정보 관련 API")
 public class ReadStatusController {
 
-  private final ReadStatusService readStatusService;
+    private final ReadStatusService readStatusService;
 
-  // [ ] 특정 채널의 메시지 수신 정보 생성
-  @PostMapping
-  public ResponseEntity<ReadStatus> createReadStatus(
-      @RequestBody ReadStatusCreateRequest readStatusCreateRequest) {
-    ReadStatus readStatus = readStatusService.createReadStatus(readStatusCreateRequest);
-    return ResponseEntity.status(HttpStatus.CREATED).body(readStatus); // 201 Created
-  }
+    // [ ] 특정 채널의 메시지 수신 정보 생성
+    @Operation(summary = "메시지 수신 정보 생성 API", responses = {
+            @ApiResponse(responseCode = "201", description = "메시지 수신 정보가 생성되었습니다."),
+            @ApiResponse(responseCode = "400", description = "잘못된 인자값이 포함되었습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @PostMapping
+    public ResponseEntity<ReadStatus> createReadStatus(
+            @RequestBody ReadStatusCreateRequest readStatusCreateRequest) {
+        ReadStatus readStatus = readStatusService.createReadStatus(readStatusCreateRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(readStatus); // 201 Created
+    }
 
-  // [ ] 특정 채널의 메시지 수신 정보 수정
-  @PatchMapping("/{readStatusId}")
-  public ResponseEntity<ReadStatus> modifyReadStatus(
-      @PathVariable UUID readStatusId,
-      @RequestBody ReadStatusUpdateRequest readStatusUpdateRequest) {
-    ReadStatus readStatus = readStatusService.updateReadStatus(readStatusId,
-        readStatusUpdateRequest);
-    return ResponseEntity.status(HttpStatus.OK).body(readStatus); // 200 OK
-  }
+    // [ ] 특정 채널의 메시지 수신 정보 수정
+    @Operation(summary = "메시지 수신 정보 수정 API", responses = {
+            @ApiResponse(responseCode = "200", description = "메시지 수신 정보가 수정되었습니다."),
+            @ApiResponse(responseCode = "400", description = "잘못된 인자값이 포함되었습니다."),
+            @ApiResponse(responseCode = "404", description = "수신 정보 ID가 일치하지 않습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @PatchMapping("/{readStatusId}")
+    public ResponseEntity<ReadStatus> modifyReadStatus(
+            @PathVariable UUID readStatusId,
+            @RequestBody ReadStatusUpdateRequest readStatusUpdateRequest) {
+        ReadStatus readStatus = readStatusService.updateReadStatus(readStatusId, readStatusUpdateRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(readStatus); // 200 OK
+    }
 
-  // [ ] 특정 사용자의 메시지 수신 정보 조회
-  @GetMapping
-  public ResponseEntity<List<ReadStatus>> findReadStatusByUserId(@RequestParam UUID userId) {
-    List<ReadStatus> readStatuses = readStatusService.findAllByUserId(userId);
-    return ResponseEntity.status(HttpStatus.OK).body(readStatuses); // 200 OK
-  }
+    // [ ] 특정 사용자의 메시지 수신 정보 조회
+    @Operation(summary = "사용자별 메시지 수신 정보 조회 API", responses = {
+            @ApiResponse(responseCode = "200", description = "메시지 수신 정보가 정상적으로 조회되었습니다."),
+            @ApiResponse(responseCode = "400", description = "잘못된 사용자 ID가 포함되었습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @GetMapping
+    public ResponseEntity<List<ReadStatus>> findReadStatusByUserId(@RequestParam UUID userId) {
+        List<ReadStatus> readStatuses = readStatusService.findAllByUserId(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(readStatuses); // 200 OK
+    }
 }
