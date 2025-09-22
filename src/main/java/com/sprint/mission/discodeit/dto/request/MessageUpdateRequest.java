53c5dc004ae31d8f96a475e1812c28774868635c
Namsoo315 <gustn2130@naver.com>
2025-09-22 16:15:40 +0900

    fix: Validator 수정

--- src/main/java/com/sprint/mission/discodeit/dto/request/MessageUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/MessageUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.sprint.mission.discodeit.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record MessageUpdateRequest(
