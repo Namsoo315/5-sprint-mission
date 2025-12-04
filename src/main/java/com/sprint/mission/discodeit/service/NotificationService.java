41b968d72bac408e4724cc3d64e59e35152c0059
Namsoo315 <gustn2130@naver.com>
2025-12-04 18:04:33 +0900

    feat: Notification Mapper, Service 추가

---
+++ src/main/java/com/sprint/mission/discodeit/service/NotificationService.java
@@ -0,0 +1,12 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.dto.data.NotificationDTO;
+import java.util.List;
+import java.util.UUID;
+
+public interface NotificationService {
+
+  List<NotificationDTO> findNotifications(UUID receiverId);
+
+  void checkNotifications(UUID notificationId);
+}
