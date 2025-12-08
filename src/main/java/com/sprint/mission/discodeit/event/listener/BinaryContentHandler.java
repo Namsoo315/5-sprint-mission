93db7bdabeceb99cee8f81d4ca4b435b2f794c9e
Namsoo315 <gustn2130@naver.com>
2025-12-08 17:40:15 +0900

    feat: Kafka로 비동기 처리 변경

--- src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentHandler.java
+++ src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentHandler.java
@@ -1,18 +1,15 @@
 package com.sprint.mission.discodeit.event.listener;
 
 import com.sprint.mission.discodeit.entity.BinaryContentStatus;
-import com.sprint.mission.discodeit.entity.Notification;
-import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.event.BinaryContentCreatedEvent;
+import com.sprint.mission.discodeit.event.S3UploadFailedEvent;
 import com.sprint.mission.discodeit.exception.binarycontent.BinaryContentSaveFailedException;
-import com.sprint.mission.discodeit.exception.user.UserNotFoundException;
-import com.sprint.mission.discodeit.repository.NotificationRepository;
-import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
@@ -28,8 +25,7 @@ public class BinaryContentHandler {
 
   private final BinaryContentStorage binaryContentStorage;
   private final BinaryContentService binaryContentService;
-  private final NotificationRepository notificationRepository;
-  private final UserRepository userRepository;
+  private final ApplicationEventPublisher applicationEventPublisher;
 
   @Retryable(
       maxAttempts = 5,
@@ -52,23 +48,14 @@ public class BinaryContentHandler {
 
   @Recover
   public void recover(BinaryContentSaveFailedException e, BinaryContentCreatedEvent event) {
-    log.info("recover binary content file. id={}", event.id());
-
     binaryContentService.updateStatus(event.id(), BinaryContentStatus.FAIL);
 
-    // 굉장히 싫다 쿼리 또 쓰기?
-    User user = userRepository.findByUsername("admin").orElseThrow(
-        UserNotFoundException::new);
+    S3UploadFailedEvent failedEvent = new S3UploadFailedEvent(
+        event.id(),
+        MDC.get("requestId"),
+        e.getMessage()
+    );
 
-    String requestId = MDC.get("requestId");
-    Notification notification = Notification.builder()
-        .user(user)// 어드민 한테 보내기
-        .title("S3 파일 업로드 실패")
-        .content(
-            "RequestId: " + requestId + "\nBinaryContentId: " + event.id()
-                + "\nError: " + e.getMessage())
-        .build();
-
-    notificationRepository.save(notification);
+    applicationEventPublisher.publishEvent(failedEvent);
   }
 }
