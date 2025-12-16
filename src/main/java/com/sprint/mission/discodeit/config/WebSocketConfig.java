67bfae049b14d06af0f4f2f51210ca548206bc8d
Namsoo315 <gustn2130@naver.com>
2025-12-16 14:08:51 +0900

    feat: 분산환경에서 메시징 웹소켓 인증/인가 처리를 위한 기능 추가

--- src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -1,14 +1,24 @@
 package com.sprint.mission.discodeit.config;
 
+import com.sprint.mission.discodeit.entity.UserRole;
+import com.sprint.mission.discodeit.security.jwt.JwtAuthenticationChannelInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  private final JwtAuthenticationChannelInterceptor jwtAuthenticationChannelInterceptor;
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -26,4 +36,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         .withSockJS();
   }
 
+  @Override
+  public void configureClientInboundChannel(ChannelRegistration registration) {
+    registration.interceptors(
+        jwtAuthenticationChannelInterceptor,
+        new SecurityContextChannelInterceptor(),
+        authorizationChannelInterceptor()
+    );
+  }
+
+  private AuthorizationChannelInterceptor authorizationChannelInterceptor() {
+    return new AuthorizationChannelInterceptor(
+        MessageMatcherDelegatingAuthorizationManager.builder()
+            .anyMessage().hasRole(UserRole.USER.name())
+            .build()
+    );
+  }
 }
